af1f5bbbde8f1903fffed1e84f79f81d6747f485
irfan <irfan@irfans-MacBook-Pro.local>
2026-09-05 23:50:56 +0900

    week 1 complete basic of python .

---
+++ week_01/scripts1.py
@@ -0,0 +1,45 @@
+# Topics
+
+# 1. Writing first program
+# 2. print statement
+# 3. Comments
+# 4. Escape sequences
+# 5. Debugging Basics 
+# 6. Small Practice Excercises 
+
+# 7. Functions > print()
+
+
+
+# print("Hello Muhammad Irfan Generative Ai Developer...", "Wellcome to class 1 of python for generative ai..", "Lets start..", sep="\n")
+# print("Wellcome to class 1 of python for generative ai..")
+
+# print("=" * 70)
+
+# print("I am learning a GenAI Development..")
+
+
+# Escape sequences 
+# \n , \t, 
+
+# name = " Jon wick"
+# age = 35
+# height = 5.9
+# print( name, age, height, "feet ", sep= "--------")
+
+
+int1 = 50 
+int2 = 100 
+
+result = int1 + int2
+
+# print(f"My first number is {int1}", f"My second number is {int2}", f"here is the result {result}", sep="\n")
+
+
+print("=" * 50, "\t MY PROFIlE", "=" * 50, 
+      "Name : \tHamza", 
+      "Age ; \t21", 
+      "City : \tOkara", 
+      "Skill :\tPython",
+      "=" * 50, sep="\n")
+
