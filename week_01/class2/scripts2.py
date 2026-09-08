5424ff1fef78257f6f4d03ed4d33772b7f9d7f0b
irfan <irfan@irfans-MacBook-Pro.local>
2026-09-08 10:42:18 +0900

    The age of user added

---
+++ week_01/class2/scripts2.py
@@ -0,0 +1,46 @@
+# Topics
+
+# 1. Variables 
+# 2. Data Types
+# 3. user Input
+# 4. Type Casting
+# 5. Operators
+# 6. String
+# 7. String Methods
+# 8. Docstrings
+
+# ====================================================
+
+# 1. Variables 
+
+# name = "Irfan"
+# age  = 29
+# weight = 50
+
+# print(name, age, weight)
+
+# ====================================================
+
+# 2. Data Types
+
+name = "Irfan"
+age  = 29
+weight = 50.80
+does_smoking = False # Boolean Value True/False
+
+# # Display Result
+# print(name, age, weight)
+# print(type(age))
+# print(type(name))
+# print(type(weight))
+# print(type(does_smoking))
+
+# ====================================================
+
+# 3. user Input
+
+user1 = str(input("Please Enter Your name: "))
+age = str(input("Please Enter your age "))
+
+print(user1)
+print("Thankyou for using our VS code ID for becoomg a Pro Level GenAI Developer.")
